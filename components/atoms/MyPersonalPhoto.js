2e8abe0e0a74f20ce1d932f9458ac4fbe929060d
nerea pardo <nerea.pardo.cundins@gmail.com>
2022-01-11 10:09:36 +0000

    wip

--- components/atoms/MyPersonalPhoto.js
+++ components/atoms/MyPersonalPhoto.js
@@ -2,8 +2,16 @@ import Image from "next/image";
 import me from "./../../public/images/me.jpg";
 const MyPersonalPhoto = () => {
   return (
-    <div>
-      <Image src={me} alt="Nerea Pardo Cundíns" />
+    <div className="border-8 border-double border-neon rounded-full w-fit h-fit">
+      <div className="h-64 w-64 relative rounded-full">
+        <Image
+          src={me}
+          alt="Photo about me"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+        />
+      </div>
     </div>
   );
 };
