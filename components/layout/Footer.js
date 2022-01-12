e6e213b14dd895850c54fd7bacb74b984f80c5fc
nerea pardo <nerea.pardo.cundins@gmail.com>
2022-01-12 10:59:46 +0000

    wip

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,4 +1,8 @@
 const Footer = () => {
-  return <div className="bg-neon">About me</div>;
+  return (
+    <div className="bg-stone-700 text-neon border-t-2 border-neon">
+      About me
+    </div>
+  );
 };
 export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,24 @@ export default function Home() {
       </Head>
       <div className="bg-stone-900 flex flex-col items-center pt-12 text-white">
         <MyPersonalPhoto />
-        <h1 className="mt-6 font-light">
-          Hi, I am <span className="font-medium">Nerea Pardo Cundíns</span>
+        <h1 className="mt-6">
+          Hi, I am <span className="font-bold">Nerea Pardo Cundíns</span>
         </h1>
         <h2 className="font-light">Nice to meet you!</h2>
+        <div className="mx-8">
+          <p className="font-light my-8 text-stone-400">
+            I am a Full-Stack web developer and my motivation is to help people
+            through quality software that can make their lives easier.
+          </p>
+          <div className="flex justify-around">
+            <button className="rounded-3xl border-2 border-neon py-1 px-4 text-sm font-light text-neon">
+              My resume
+            </button>
+            <button className="rounded-3xl bg-neon py-1 px-4 text-sm text-slate-900">
+              More about me &#10141;
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
