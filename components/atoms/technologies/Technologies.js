72f49479d0c0f58ca5edf49daba1e4429b0499ee
nerea pardo <nerea.pardo.cundins@gmail.com>
2022-01-15 20:03:47 +0000

    fix technologies component for responsive design

--- components/atoms/technologies/Technologies.js
+++ components/atoms/technologies/Technologies.js
@@ -1,11 +1,11 @@
 const Technologies = ({ technologies, className = "" }) => {
   return (
-    <div className={`flex h-10 ${className}`}>
-      <div className="bg-neon w-1/2 pl-9 flex items-center text-xl">
+    <div className={`flex h-16 lg:h-10 ${className}`}>
+      <div className="bg-neon w-9/12 lg:w-1/2 pl-9 flex items-center text-xl">
         Technologies: {technologies}
       </div>
-      <div className="w-11  overflow-hidden inline-block">
-        <div className=" h-16  bg-neon rotate-45 transform origin-top-right"></div>
+      <div className="w-16 lg:w-11  overflow-hidden inline-block">
+        <div className="h-32 lg:h-16  bg-neon rotate-45 transform origin-top-right"></div>
       </div>
     </div>
   );
